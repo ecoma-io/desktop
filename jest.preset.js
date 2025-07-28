cfd2936d5c05a399987a10536e3924860155f56f
John Martin <github-actions[bot]@users.noreply.github.com>
2025-07-29 02:11:03 +0700

    chore: update configuration for jest and add coverage settings

--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,22 @@
 const nxPreset = require('@nx/jest/preset').default;
+const isCI = require('is-ci');
 
 module.exports = {
   ...nxPreset,
-  passWithNoTests: true
- };
+  ci: isCI,
+  passWithNoTests: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+  ],
+  coverageDirectory: './coverage',
+  coverageReporters: ['text'],
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+};
