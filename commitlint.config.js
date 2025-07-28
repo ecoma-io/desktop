1ec0888ebda312d33361762dd1113622358a0781
John Martin <github-actions[bot]@users.noreply.github.com>
2025-07-29 02:23:27 +0700

    chore: update commitlint configuration and add coding standards and commit conventions documentation

--- commitlint.config.js
+++ commitlint.config.js
@@ -23,7 +23,7 @@ module.exports = {
     'subject-case': [2, 'always', 'lowerCase'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
   },
